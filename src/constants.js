aab8bcd2c50f00b7bf63a3c6d51b51db7c655178
mayank7enn <96970037+mayank7enn@users.noreply.github.com>
2024-07-21 05:49:29 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -5,9 +5,9 @@ export const dialogueData = {
     I also like C++ and Python Languages . Anyway regardless of the language, I just like programming.
     Here is my <a href="https://github.com/mayank7enn" target="_blank">Github</a>!`,
   "cs-degree": `This is my IT degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `I also enjoy making game programming tutorials and sharing my knowledge with others. 
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
   If you're interested in learning how I built this portfolio you're currently playing through, 
-  check out some of the tutorials and resources available online about game development and the Kaboom.js library!`,
+  check out some of the resources available online about game development and the Kaboom.js library!`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
    <a href="https://www.youtube.com/@chaiaurcode" target="_blank">Chai aur code</a>, <a href="https://www.youtube.com/@NitishRajput" target="_blank">Nitish Rajput</a>,
   <a href="https://www.youtube.com/@mkbhd/videos" target="_blank">MKBHD</a> (sometimes) and <a href="https://https://www.youtube.com/@ThinkSchool" target="_blank">Think School</a>!`,
@@ -15,7 +15,7 @@ export const dialogueData = {
   resume: `This is my desk and on it is my resume. <a href="https://github.com/mayank7enn/resume/blob/main/Mayank-resume.pdf" target="_blank">Check it out?</a>
   Contact me at mayank7en@gmail.com if you have any interesting job opportunities!`,
   projects: ` This portfolio is made with the Kaboom.js library, a powerful tool for making games in JavaScript.
-  Learn more about how to use Kaboom.js by exploring the official documentation <a href="https://kaboomjs.com/" target="_blank">Kaboom.js website</a><br>
+  Learn more about how to use Kaboom.js by exploring documentation <a href="https://kaboomjs.com/" target="_blank">Kaboom.js website</a><br>
   Below are links to my other projects:<br>
   <a href="https://youtube-clone-henna-tau-55.vercel.app/" target="_blank">YouTube Clone</a>,
   <a href="https://appwriteblog-gamma.vercel.app/" target="_blank">Blog</a>,
