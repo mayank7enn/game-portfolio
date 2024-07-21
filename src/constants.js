5cfb2c8df255cd1af9e2559eff8698d98622aa2a
mayank7enn <96970037+mayank7enn@users.noreply.github.com>
2024-07-21 05:42:24 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in ReactJS these days.
+    I also like C++ and Python Languages . Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/mayank7enn" target="_blank">Github</a>!`,
+  "cs-degree": `This is my IT degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `I also enjoy making game programming tutorials and sharing my knowledge with others. 
+  If you're interested in learning how I built this portfolio you're currently playing through, 
+  check out some of the tutorials and resources available online about game development and the Kaboom.js library!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@chaiaurcode" target="_blank">Chai aur code</a>, <a href="https://www.youtube.com/@NitishRajput" target="_blank">Nitish Rajput</a>,
+  <a href="https://www.youtube.com/@mkbhd/videos" target="_blank">MKBHD</a> (sometimes) and <a href="https://https://www.youtube.com/@ThinkSchool" target="_blank">Think School</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/mayank7enn/resume/blob/main/Mayank-resume.pdf" target="_blank">Check it out?</a>
+  Contact me at mayank7en@gmail.com if you have any interesting job opportunities!`,
+  projects: ` This portfolio is made with the Kaboom.js library, a powerful tool for making games in JavaScript.
+  Learn more about how to use Kaboom.js by exploring the official documentation <a href="https://kaboomjs.com/" target="_blank">Kaboom.js website</a><br>
+  Below are links to my other projects:<br>
+  <a href="https://youtube-clone-henna-tau-55.vercel.app/" target="_blank">YouTube Clone</a>,
+  <a href="https://appwriteblog-gamma.vercel.app/" target="_blank">Blog</a>,
+  and many more to come...`,
+  library: `There are a lot of programming books on my shelves.
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit Mayank's portfolio, just close the tab.`,
+};
